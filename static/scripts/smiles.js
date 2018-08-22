07ed8eb3908eca3e82a778f6fdfc9cb5bd536acd
arslanay <arslanay@eecs.wsu.edu>
2018-08-22 10:37:54 -0700

    Updating the backend URL.

--- static/scripts/smiles.js
+++ static/scripts/smiles.js
@@ -5,7 +5,8 @@ var Smile = (function() {
         
     // The backend we'll use for Part 2. For Part 3, you'll replace this 
     // with your backend.
-    var apiUrl = 'https://smile451.herokuapp.com'; 
+    // var apiUrl = 'https://smile451.herokuapp.com';  //Ruby on Rails backend
+    var apiUrl = 'https://arslanay-warmup.herokuapp.com'      //Flask-Python backend
     // FINISH ME (Task 4): You can use the default smile space, but this means
     //            that your new smiles will be merged with everybody else's
     //            which can get confusing. Change this to a name that 
